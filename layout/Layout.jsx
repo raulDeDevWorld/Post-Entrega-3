4f437b7a0490c7b9ebb0f98742eb179b19d7f7fd
Your Name <you@example.com>
2023-05-02 23:09:08 -0400

    DC

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -25,7 +25,7 @@ import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
 function Layout({ children }) {
-    const { userDB, setUserData, monthAndYear, setUserSuccess, success, postsIMG,setUserPostsIMG, showImg, date, setUserDate } = useUser()
+    const { userDB, setUserData, monthAndYear, setUserSuccess, success, postsIMG, setUserPostsIMG, showImg, date, setUserDate } = useUser()
 
     const [dataEditor, setDataEditor] = useState(null)
 
@@ -45,11 +45,11 @@ function Layout({ children }) {
     }
     useEffect(() => {
         // let arr2 = ['BannerIzquierdo1', 'BannerIzquierdo2', 'BannerIzquierdo3', 'BannerIzquierdo4','BannerPortada1', 'BannerPortada2', 'BannerPortada3', 'BannerDerecho1', 'BannerDerecho2', 'BannerDerecho3', 'BannerDerecho4','BannerPortada']
-       
+
         // arr2.map((i) => { 
         //     userDB && userDB[i] && getIndexStorage('Banners', userDB[i], postsIMG, setUserPostsIMG)
         // });
-        
+
     }, [userDB]);
 
 
@@ -66,67 +66,72 @@ function Layout({ children }) {
                 <BannerRight carpeta="BannerDerecho" items={[1, 2, 3, 4]} click={handlerClickEnlace}></BannerRight>
             </div>
             <main className={styles.main}>
-                    {children}
+                {children}
 
-                    <footer className={styles.footer} id="Nosotros">
-                        <div>
-                            <h5>MISIÓN</h5>
-                            <div className={styles.footerItemsContainer}>
-                                <img src="/vision.svg" alt="" />
-                                <p className={styles.paragraph}>Informar, educar y contribuir a la formación de una cultura ciudadana en torno a la realidad nacional e internacional.</p>
-
-                            </div>
-                        </div>
-                        <div>
-                            <h5>DIRECCIÓN Y PUBLICIDAD ONLINE</h5>
-                            <div className={styles.footerItemsContainer}>
-                                <img src="/contact.svg" alt="" />
-                                <p>(+591) 2 488973 <br /> 73002076 <br />60101760</p>
-                                <img src="/ubication.svg" alt="" />
-                                <p>Calle Cañada Strongest, <br /> No. 1782 esq. Capitán Castrillo, <br /> Edif. Napolis, Piso 6, Of. 6B <br /> Zona San Pedro</p>
-                            </div>
+                <footer className={styles.footer} id="Nosotros">
+                    <div>
+                        <h5>MISIÓN</h5>
+                        <div className={styles.footerItemsContainer}>
+                            <img src="/vision.svg" alt="" />
+                            <p className={styles.paragraph}>Informar, educar y contribuir a la formación de una cultura ciudadana en torno a la realidad nacional e internacional.</p>
 
                         </div>
-                        <div>
-                            <h5>VISIÓN</h5>
-                            <div className={styles.footerItemsContainer}>
-                                <img src="/mision.svg" alt="" />
-                                <p>Ser el medio impreso y digital de mayor influencia en la construcción de un cultura ciudadana en torno a la realidad nacional e internacional.</p>
-                            </div>
+                    </div>
+                    <div>
+                        <h5>DIRECCIÓN Y PUBLICIDAD ONLINE</h5>
+                        <div className={styles.footerItemsContainer}>
+                            <img src="/contact.svg" alt="" />
+                            <p>(+591) 2 488973 <br /> 73002076 <br />60101760</p>
+                            <img src="/ubication.svg" alt="" />
+                            <p>Calle Cañada Strongest, <br /> No. 1782 esq. Capitán Castrillo, <br /> Edif. Napolis, Piso 6, Of. 6B <br /> Zona San Pedro</p>
                         </div>
-                        <div>
-                            <h5>DIRECCIÓN</h5>
-                            <div className={styles.socialMediaIcons}>
-                                <Link href="https://www.facebook.com/periodicohoybolivia0" legacyBehavior scroll={false}>
-                                    <a onClick={handlerClick} target="_blank"><img src="/SocialMedia/FACEBOOK-01.svg" alt="SocialMedia" /></a>
-                                </Link>
-                                <Link href="https://www.instagram.com/periodicohoybolivia/" legacyBehavior scroll={false}>
-                                    <a onClick={handlerClick} target="_blank"><img src="/SocialMedia/INSTAGRAM-02.svg" alt="SocialMedia" /></a>
-                                </Link>
-                                <Link href="https://twitter.com/_HOYBolivia" legacyBehavior scroll={false}>
-                                    <a onClick={handlerClick} target="_blank"> <img src="/SocialMedia/TWITTER-03.svg" alt="SocialMedia" /></a>
-                                </Link>
-                                <Link href="https://www.youtube.com/channel/UCXFA6pzESb1NQMsepmhC6Vw" legacyBehavior scroll={false}>
-                                    <a onClick={handlerClick} target="_blank"> <img src="/SocialMedia/YOUTUBE-04.svg" alt="SocialMedia" /></a>
-                                </Link>
-                                <Link href="https://www.tiktok.com/@periodicohoybolivia" legacyBehavior scroll={false}>
-                                    <a onClick={handlerClick} target="_blank"> <img src="/SocialMedia/tIK tOK-05.svg" alt="SocialMedia" /></a>
-                                </Link>
-                                <Link href={`https://api.whatsapp.com/send?phone=+59161116665&text=Hola%20Periódico%20HOY%20%20quiero%20contactarme%20con%20un%20agente%20de%20ventas...`} legacyBehavior scroll={false}>
-                                    <a onClick={handlerClick} target="_blank"> <img src="/SocialMedia/WHATSAPP-06.svg" alt="SocialMedia" /></a>
-                                </Link>
-                            </div>
 
+                    </div>
+                    <div>
+                        <h5>VISIÓN</h5>
+                        <div className={styles.footerItemsContainer}>
+                            <img src="/mision.svg" alt="" />
+                            <p>Ser el medio impreso y digital de mayor influencia en la construcción de un cultura ciudadana en torno a la realidad nacional e internacional.</p>
                         </div>
+                    </div>
+                    <div>
+                        <h5>DIRECCIÓN</h5>
+                        <div className={styles.socialMediaIcons}>
+                            <Link href="https://www.facebook.com/periodicohoybolivia0" legacyBehavior scroll={false}>
+                                <a onClick={handlerClick} target="_blank"><img src="/SocialMedia/FACEBOOK-01.svg" alt="SocialMedia" /></a>
+                            </Link>
+                            <Link href="https://www.instagram.com/periodicohoybolivia/" legacyBehavior scroll={false}>
+                                <a onClick={handlerClick} target="_blank"><img src="/SocialMedia/INSTAGRAM-02.svg" alt="SocialMedia" /></a>
+                            </Link>
+                            <Link href="https://twitter.com/_HOYBolivia" legacyBehavior scroll={false}>
+                                <a onClick={handlerClick} target="_blank"> <img src="/SocialMedia/TWITTER-03.svg" alt="SocialMedia" /></a>
+                            </Link>
+                            <Link href="https://www.youtube.com/channel/UCXFA6pzESb1NQMsepmhC6Vw" legacyBehavior scroll={false}>
+                                <a onClick={handlerClick} target="_blank"> <img src="/SocialMedia/YOUTUBE-04.svg" alt="SocialMedia" /></a>
+                            </Link>
+                            <Link href="https://www.tiktok.com/@periodicohoybolivia" legacyBehavior scroll={false}>
+                                <a onClick={handlerClick} target="_blank"> <img src="/SocialMedia/tIK tOK-05.svg" alt="SocialMedia" /></a>
+                            </Link>
+                            <Link href={`https://api.whatsapp.com/send?phone=+59161116665&text=Hola%20Periódico%20HOY%20%20quiero%20contactarme%20con%20un%20agente%20de%20ventas...`} legacyBehavior scroll={false}>
+                                <a onClick={handlerClick} target="_blank"> <img src="/SocialMedia/WHATSAPP-06.svg" alt="SocialMedia" /></a>
+                            </Link>
+                        </div>
+
+                    </div>
+                    <div>
+
                         <Link href="/Login" legacyBehavior scroll={false}>
-                                    <a onClick={handlerClick}> <span> ©TARKAN Ltda.</span></a>
+                            <a onClick={handlerClick}> <span> ©TARKAN Ltda.</span></a>
 
                         </Link>
                         <Link href="https://swoou.com" legacyBehavior scroll={false}>
-                                    <a onClick={handlerClick}> <span> Desarrollado por Swoou.com</span></a>
+                            <a onClick={handlerClick}> <span> Desarrollado por Swoou.com</span></a>
                         </Link>
-                       
-                    </footer>
+
+                    </div>
+
+
+                </footer>
 
 
 
